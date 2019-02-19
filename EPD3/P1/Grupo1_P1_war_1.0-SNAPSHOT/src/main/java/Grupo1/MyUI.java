ba4c60b52aecd134aa2ac445348e3ffcc5ceace2
Victor-martinelli <vicbomlen@gmail.com>
2019-02-19 22:57:50 +0100

    P1 WIP

--- EPD3/P1/Grupo1_P1_war_1.0-SNAPSHOT/src/main/java/Grupo1/MyUI.java
+++ EPD3/P1/Grupo1_P1_war_1.0-SNAPSHOT/src/main/java/Grupo1/MyUI.java
@@ -7,15 +7,14 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import org.apache.commons.lang3.StringUtils;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.VerticalSplitPanel;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -25,8 +24,12 @@ import com.vaadin.ui.VerticalSplitPanel;
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
-public class MyUI extends UI {
-
+public class MyUI extends UI implements Button.ClickListener{
+    
+    public GridLayout grid;
+    public Integer resultado;
+    public Label resultadoEtiq;
+    
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         /*
@@ -45,8 +48,7 @@ public class MyUI extends UI {
         layout.setMargin(true);
         layout.setSpacing(true);
         */
-        
-        GridLayout grid = new GridLayout(4,1);
+        grid = new GridLayout(4,1);
         
         HorizontalLayout layoutNumbers1 = new HorizontalLayout();
         
@@ -56,14 +58,16 @@ public class MyUI extends UI {
         grid.addComponent(layoutNumbers1,0,0);
         
         VerticalLayout layoutNumbers2 = new VerticalLayout();
+
         createNumbers(layoutNumbers2);
         grid.addComponent(layoutNumbers2,1,0);
         
         HorizontalLayout operationLayout = new HorizontalLayout();
-        operationLayout.addComponent(new Button("+"));
-        operationLayout.addComponent(new Button("-"));
-        operationLayout.addComponent(new Button("/"));
-        operationLayout.addComponent(new Button("*"));
+        
+        operationLayout.addComponent(createOperationButton("+"));
+        operationLayout.addComponent(createOperationButton("-"));
+        operationLayout.addComponent(createOperationButton("*"));
+        operationLayout.addComponent(createOperationButton("/"));
         
         grid.addComponent(operationLayout, 2, 0);
         
@@ -74,10 +78,55 @@ public class MyUI extends UI {
     {
         for (int i = 0; i <=9; i++) {
             Button button = new Button(Integer.toString(i));
-            button.
+            button.addListener(this);
            layout.addComponent(button);
         }
     }
+    
+
+    // application.
+    public void buttonClick(ClickEvent event) {
+
+        Button button = event.getButton();
+
+        String valorBotonPulsado = Character.toString(button.getCaption().charAt(0));
+
+        // Calculate the new value
+        int newValue = realizarOperacion(valorBotonPulsado);
+
+        // Update the result label with the new value
+        display.setValue(newValue);
+
+    }
+    
+    protected int realizarOperacion(String valorBotonPulsado)
+    {
+        
+        //Si pulsamos un boton de numero
+        if(StringUtils.isNumeric(valorBotonPulsado))
+        {
+            
+        }
+        
+    }
+    
+    
+    
+    protected Button createOperationButton(String value)
+    {
+        Button operation = new Button(value);
+        
+        operation.addClickListener(new Button.ClickListener() { 
+                public void buttonClick(ClickEvent event) {
+                operacion=value;
+                }
+            }
+            );
+        
+        return operation;
+    }
+    
+    
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)

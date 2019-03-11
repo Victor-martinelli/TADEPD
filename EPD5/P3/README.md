37e3b408e95bd45541816bed62e1a8f65e3c163d
Victor-martinelli <vicbomlen@gmail.com>
2019-03-11 18:24:34 +0100

    Uploaded new p3 WIP

--- EPD5/P3/README.md
+++ EPD5/P3/README.md
@@ -1,4 +1,4 @@
-P4
+P2
 ==============
 
 Template for a simple Vaadin application that only requires a Servlet 3.0 container to run.
--- EPD5/P3/target/classes/VAADIN/widgetsets/AppWidgetset/deferredjs/9E6D1E976B030DEC5C3E2B3553D26D6E/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.AppWidgetset.runAsyncCallback2('hgb(1650,1,V9d);_.vc=function Npc(){x9b((!q9b&&(q9b=new D9b),q9b),this.a.d)};K2d(Zh)(2);\n//# sourceURL=AppWidgetset-2.js\n')
